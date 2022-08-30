8d991d11c621d62c75c626b9ce17ea05fda5f203
shea <shea825@outlook.com>
2022-08-30 11:15:50 +0800

    first commit

---
+++ muduo/base/tests/Exception_test.cc
@@ -0,0 +1,46 @@
+#include "muduo/base/CurrentThread.h"
+#include "muduo/base/Exception.h"
+#include <functional>
+#include <stdio.h>
+#include <vector>
+
+class Bar {
+public:
+  void test(std::vector<std::string> names = {}) {
+    printf("Stack:\n%s\n", muduo::CurrentThread::stackTrace(true).c_str());
+    [] {
+      printf("Stack inside lambda:\n%s\n",
+             muduo::CurrentThread::stackTrace(true).c_str());
+    }();
+    std::function<void()> func([] {
+      printf("Stack inside std::function:\n%s\n",
+             muduo::CurrentThread::stackTrace(true).c_str());
+    });
+    func();
+
+    func = std::bind(&Bar::callback, this);
+    func();
+
+    throw muduo::Exception("oops");
+  }
+
+private:
+  void callback() {
+    printf("Stack inside std::bind:\n%s\n",
+           muduo::CurrentThread::stackTrace(true).c_str());
+  }
+};
+
+void foo() {
+  Bar b;
+  b.test();
+}
+
+int main() {
+  try {
+    foo();
+  } catch (const muduo::Exception &ex) {
+    printf("reason: %s\n", ex.what());
+    printf("stack trace:\n%s\n", ex.stackTrace());
+  }
+}
